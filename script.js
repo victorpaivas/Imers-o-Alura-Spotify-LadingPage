5e7bc134ac1db38df16ee0853f2dfedc4d646aba
Victor Paiva <129319485+victorpaivas@users.noreply.github.com>
2024-01-29 15:38:00 -0300

    Feat added artist search functionality dynamically via a json api

--- script.js
+++ script.js
@@ -3,13 +3,24 @@ const resultsArtist = document.getElementById('result-artist');
 const resultPlayList = document.getElementById('result-playlists')
 
 function requestApi(searchTerm) {
-    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`;
     fetch(url)
     .then((response) => response.json())
-    //.then((result) => displayResults(result))
+    .then((result) => displayResults(result))
 }
 
+function displayResults(result) {
+    resultPlayList.classList.add('hidden');
+    const artistName = document.getElementById('artist-name');
+    const artistImage = document.getElementById('artist-img');
 
+    result.forEach(element => {
+        artistName.innerText = element.name;
+        artistImage.src = element.urlImg;
+    });
+
+    resultsArtist.classList.remove('hidden')
+}
 
 document.addEventListener('input', () => {
     const  searchTerm = searchInput.value.toLowerCase();
